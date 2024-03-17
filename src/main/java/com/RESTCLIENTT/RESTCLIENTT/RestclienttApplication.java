4a9c6c095635293bc4e934f248835b7e8d91ec3d
Mayuri Lad <mayurilad@L-FVFHM72AQ05D.local>
2024-03-17 13:16:11 +0530

    rest client for config server

---
+++ src/main/java/com/RESTCLIENTT/RESTCLIENTT/RestclienttApplication.java
@@ -0,0 +1,13 @@
+package com.RESTCLIENTT.RESTCLIENTT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestclienttApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestclienttApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/RESTCLIENTT/RESTCLIENTT/RestclienttApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RESTCLIENTT.RESTCLIENTT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestclienttApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
